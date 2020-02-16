4a9654a8831260bd56c39bf58708407548401a8f
Saksham Mittal <181210045@nitdelhi.ac.in>
2020-02-16 14:00:38 +0530

    corrected the compare list quantity

--- core/templatetags/library_template_tag.py
+++ core/templatetags/library_template_tag.py
@@ -7,19 +7,19 @@ register = template.Library()
 @register.filter
 def added_to_bookmark(user, id):
     if user.is_authenticated:
-        qs = bookmark.objects.filter(user=user)
-        if qs.exists():
-            if qs.libraries.filter(id=id).exists():
-                return True
+        qs = bookmark.objects.filter(user=user)[0]
+        # if qs.exists():
+        if qs.libraries.filter(id=id).exists():
+            return True
     return False
 
 
 @register.filter
 def added_to_compare(user, id):
     if user.is_authenticated:
-        qs = comaprison.objects.filter(user=user)
-        if qs.exists():
-            if qs.libraries.filter(id=id).exists():
-                return True
+        qs = comaprison.objects.filter(user=user)[0]
+        # if qs.exists():
+        if qs.libraries.filter(id=id).exists():
+            return True
     return False
 
